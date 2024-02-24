7329297b551daccfb0ae5b61437cd6aae1595287
franciscogrimes <fgsieg1831@gmail.com>
2024-02-24 00:50:06 -0300

    Ex 7 - JavaScript básico | Ex 9 - Integração Git/GitHub

---
+++ script.js
@@ -0,0 +1,66 @@
+alert("Bem vindo à mina página pessoal");
+
+// const info = document
+//   .getElementById("botaoInfo")
+//   .addEventListener("click", mostraInfo);
+// const algoritmo = document
+//   .getElementById("botaoAlg")
+//   .addEventListener("click", mostraAlg);
+// // Repita para os outros botões...
+
+function mostraInfo() {
+  alert(
+    "Curso de Informática\n" +
+      "Período: 2 meses\n" +
+      "Descrição:\n" +
+      "Curso em que foi contemplado todos os assuntos refernetes a Harware e pacote Office. "
+  );
+}
+function mostraAlg() {
+  alert(
+    "Curso de Algoritmo\n" +
+      "Período: 2 meses\n" +
+      "Descrição:\n" +
+      "Curso realizado online com o professor Gustavo Guanabar do curso em vídeo. Foi aprendido a lógica de programação, bem como algumas informações úteis para um programador. Toda a lógica foi em Portugol. "
+  );
+}
+function mostraPy() {
+  alert(
+    "Curso de Python\n" +
+      "Período: 3 meses\n" +
+      "Descrição:\n" +
+      "Curso realizado pela plataforma de ensino Udemy. Foi realizado pelo professor Luiz Otávio Miranda e pude desenvolver a minha programação em Python. "
+  );
+}
+function mostraHTML() {
+  alert(
+    "Curso de HTML5\n" +
+      "Período: 1 semana\n" +
+      "Descrição:\n" +
+      "Realizado pela plataforma OneBitCode"
+  );
+}
+function mostraCSS() {
+  alert(
+    "Curso de CSS3\n" +
+      "Período: 1 semana\n" +
+      "Descrição:\n" +
+      "Realizado pela plataforma OneBitCode"
+  );
+}
+function mostraPa() {
+  alert(
+    "Curso de Persona avançado\n" +
+      "Período: 3 dias\n" +
+      "Descrição:\n" +
+      "Curso realizado na empresa Dígitro. Essa é uma plataforma de desenvolvimento de URAs e Chatbot, com integrações de API externa e baseada em Lua."
+  );
+}
+function mostraPd() {
+  alert(
+    "Cursos dos Produtos Dígitro\n" +
+      "Período: 1 mês\n" +
+      "Descrição:\n" +
+      "Cursos realizados na empresa Dígitro.Certificando total capacitação em todas as soluções de comunicação da empresa."
+  );
+}
